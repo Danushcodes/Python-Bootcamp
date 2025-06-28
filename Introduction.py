40ba713cb30ddd4b55b18b77e3a13ca7023066fd
Danushcodes <danushcodes@gmail.com>
2025-06-28 12:10:40 +0530

    Add files via upload
    This simple program helps you practice Python’s input() function. Inspired by the Mad Libs game—in which you fill in blanks with random words to create a story—this script instead prompts you for personal details and then generates a brief self-introduction.

---
+++ Introduction.py
@@ -0,0 +1,9 @@
+name= input("What is your name?: ")
+age = input("How old are you?: ")
+place = input("Where are you from?: ")
+Hobby = input("What is your No.1 favourite hobby?: ")
+
+print(f"\n \n \nYour simple introduction:\n"
+      f" Hi I'm {name}, {age}yrs old "
+      f"\n I'm from {place}, "
+      f"\n and my favourite hobby is {Hobby} ")
